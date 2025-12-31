3d9862839f28638934d14af47863078ecb309d7b
agarwaljayant7 <44949431+agarwaljayant7@users.noreply.github.com>
2025-12-31 17:10:45 +0530

    Update items.js

--- node-items-app-service/routes/items.js
+++ node-items-app-service/routes/items.js
@@ -13,6 +13,7 @@ const data = [
     { id: 7, title: 'Have a plate of Meal', order: 7, completed: false, createdOn: new Date() },
     { id: 8, title: 'Have a nice morning', order: 8, completed: false, createdOn: new Date() },
     { id: 8, title: 'Have a nice breakfast', order: 9, completed: false, createdOn: new Date() },
+    { id: 9, title: 'Have a nice reading', order: 10, completed: false, createdOn: new Date() },
 ];
 
 // READ
